58617cccb2c3061383ec990461d5379456154288
Manel Puig <manel.puig@ub.edu>
2021-01-18 16:35:57 +0100

    mon2

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,18 +5,11 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("dynamixel-workbench/dynamixel_workbench")
-subdirs("dynamixel-workbench-msgs/dynamixel_workbench_msgs")
 subdirs("universal_robot/universal_robot")
 subdirs("universal_robot/universal_robots")
 subdirs("universal_robot/ur_description")
 subdirs("universal_robot/ur_e_description")
 subdirs("universal_robot/ur_msgs")
-subdirs("DynamixelSDK/ros/dynamixel_sdk")
-subdirs("DynamixelSDK/ros/dynamixel_sdk_examples")
-subdirs("dynamixel-workbench/dynamixel_workbench_toolbox")
-subdirs("dynamixel-workbench/dynamixel_workbench_controllers")
-subdirs("dynamixel-workbench/dynamixel_workbench_operators")
 subdirs("universal_robot/ur_bringup")
 subdirs("universal_robot/ur_driver")
 subdirs("universal_robot/ur_e_gazebo")
