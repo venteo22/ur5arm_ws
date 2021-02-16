e76d4b86e2c99aa0bb4eb1dc2c18544705c0c2f9
Manel Puig <manel.puig@ub.edu>
2021-02-16 13:57:40 +0100

    Tuesday3

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/mpuig/ur5arm_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/mpuig/ur5arm_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES="/home/mpuig/ur5arm_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/mpuig/ur5arm_ws/src:$ROS_PACKAGE_PATH"
