8e7e5646b63ff48c727b6955354ac2fd228c7863
Manel Puig <manel.puig@ub.edu>
2021-02-23 17:24:58 +0100

    tuesday1

--- src/ur5control/src/ur5e_joints_control1.py
+++ src/ur5control/src/ur5e_joints_control1.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+import math
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import sys
 import copy
 import rospy
@@ -17,7 +19,13 @@ display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path
 
 group_variable_values = group.get_current_joint_values()
 
-group_variable_values[1] = -1.5
+group_variable_values[0] = math.radians(0)
+group_variable_values[1] = math.radians(-90)
+group_variable_values[2] = math.radians(-90)
+group_variable_values[3] = math.radians(0)
+group_variable_values[4] = math.radians(0)
+group_variable_values[5] = math.radians(0)
+
 group.set_joint_value_target(group_variable_values)
 
 plan2 = group.plan()
@@ -25,22 +33,32 @@ plan2 = group.plan()
 rospy.sleep(5)
 group.go(wait=True)
 rospy.sleep(5)
+
 print ("Current Joint Values:")
-print (group.get_current_joint_values())
+joint_target2=group.get_current_joint_values()
+print ("q1 = "+str(round(math.degrees(joint_target2[0]),0)))
+print ("q2 = "+str(round(math.degrees(joint_target2[1]),0)))
+print ("q3 = "+str(round(math.degrees(joint_target2[2]),0)))
+print ("q4 = "+str(round(math.degrees(joint_target2[3]),0)))
+print ("q5 = "+str(round(math.degrees(joint_target2[4]),0)))
+print ("q6 = "+str(round(math.degrees(joint_target2[5]),0)))
 print ("Current Pose:")
-print (group.get_current_pose())
-
-group_variable_values[2] = 1.5
-group.set_joint_value_target(group_variable_values)
-
-plan2 = group.plan()
-
-rospy.sleep(5)
-group.go(wait=True)
-rospy.sleep(5)
-print ("Current Joint Values:")
-print (group.get_current_joint_values())
-print ("Current Pose:")
-print (group.get_current_pose())
+pose_target2=group.get_current_pose()
+qx=round(pose_target2.pose.orientation.x,2)
+qy=round(pose_target2.pose.orientation.y,2)
+qz=round(pose_target2.pose.orientation.z,2)
+qw=round(pose_target2.pose.orientation.w,2)
+print ("x = "+str(round(pose_target2.pose.position.x,1)))
+print ("y = "+str(round(pose_target2.pose.position.y,1)))
+print ("z = "+str(round(pose_target2.pose.position.z,1)))
+print ("qx = "+str(qx))
+print ("qy = "+str(qy))
+print ("qz = "+str(qz))
+print ("qw = "+str(qw))
+quat2=[qx, qy, qz, qw]
+(roll, pitch, yaw)=euler_from_quaternion(quat2)
+print ("roll = "+str(math.degrees(roll)))
+print ("pitch = "+str(math.degrees(pitch)))
+print ("yaw = "+str(math.degrees(yaw)))
 
 moveit_commander.roscpp_shutdown()
--- src/ur5control/src/ur5e_pose_control1.py
+++ src/ur5control/src/ur5e_pose_control1.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+import math
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import sys
 import copy
 import rospy
@@ -16,13 +18,18 @@ group = moveit_commander.MoveGroupCommander("manipulator")
 display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=1)
 
 pose_target = geometry_msgs.msg.Pose()
-pose_target.position.x = 0.5
+pose_target.position.x = 0.6
 pose_target.position.y = 0
-pose_target.position.z = 0.9
-pose_target.orientation.x = 0.0
-pose_target.orientation.y = 0.0
-pose_target.orientation.z = 0.0
-pose_target.orientation.w = 1.0
+pose_target.position.z = 0.8
+r=math.radians(0)
+p=math.radians(0)
+w=math.radians(90)
+quat1=quaternion_from_euler(r,p,w)
+
+pose_target.orientation.x = quat1[0]
+pose_target.orientation.y = quat1[1]
+pose_target.orientation.z = quat1[2]
+pose_target.orientation.w = quat1[3]
 group.set_pose_target(pose_target)
 
 plan1 = group.plan()
@@ -30,9 +37,32 @@ plan1 = group.plan()
 rospy.sleep(5)
 group.go(wait=True)
 rospy.sleep(5)
+
 print ("Current Joint Values:")
-print (group.get_current_joint_values())
+joint_target2=group.get_current_joint_values()
+print ("q1 = "+str(round(math.degrees(joint_target2[0]),0)))
+print ("q2 = "+str(round(math.degrees(joint_target2[1]),0)))
+print ("q3 = "+str(round(math.degrees(joint_target2[2]),0)))
+print ("q4 = "+str(round(math.degrees(joint_target2[3]),0)))
+print ("q5 = "+str(round(math.degrees(joint_target2[4]),0)))
+print ("q6 = "+str(round(math.degrees(joint_target2[5]),0)))
 print ("Current Pose:")
-print (group.get_current_pose())
+pose_target2=group.get_current_pose()
+qx=round(pose_target2.pose.orientation.x,2)
+qy=round(pose_target2.pose.orientation.y,2)
+qz=round(pose_target2.pose.orientation.z,2)
+qw=round(pose_target2.pose.orientation.w,2)
+print ("x = "+str(round(pose_target2.pose.position.x,1)))
+print ("y = "+str(round(pose_target2.pose.position.y,1)))
+print ("z = "+str(round(pose_target2.pose.position.z,1)))
+print ("qx = "+str(qx))
+print ("qy = "+str(qy))
+print ("qz = "+str(qz))
+print ("qw = "+str(qw))
+quat2=[qx, qy, qz, qw]
+(roll, pitch, yaw)=euler_from_quaternion(quat2)
+print ("roll = "+str(math.degrees(roll)))
+print ("pitch = "+str(math.degrees(pitch)))
+print ("yaw = "+str(math.degrees(yaw)))
 
 moveit_commander.roscpp_shutdown()
