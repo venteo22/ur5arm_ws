9407c08ec92a1515331fd2b606f339848637158f
Manel Puig <manel.puig@ub.edu>
2021-03-21 18:56:25 +0100

    Deleted buid & devel folders

--- build/phantom_omni/omni_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/manel/ur5arm_ws/src/phantom_omni/omni_msgs
-# Build directory: /home/manel/ur5arm_ws/build/phantom_omni/omni_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/ur5control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/manel/ur5arm_ws/src/ur5control
-# Build directory: /home/manel/ur5arm_ws/build/ur5control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
